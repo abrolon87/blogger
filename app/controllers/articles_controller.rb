0566121a8966f16f704a3863c7cd5541f82cfcc7
Amanda <main@Amandas-MacBook-Air.local>
2020-05-16 20:36:29 -0400

    added delete with confirm alert

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,5 +20,10 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy 
+    Article.find(params[:id]).destroy
+
+    redirect_to articles_path
+  end 
 
 end
